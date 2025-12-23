8509555e81885bf29361ca0de90e084339bdc748
Gemmy Adyendra <gemsdesign88@gmail.com>
2025-12-23 16:17:32 +0700

    Revert webpack config - fix middleware invocation error

--- next.config.js
+++ next.config.js
@@ -8,15 +8,6 @@ const nextConfig = {
     maxInactiveAge: 25 * 1000,
     pagesBufferLength: 5,
   },
-  webpack: (config, { isServer }) => {
-    // Fix React version mismatch - ensure Next.js uses app's React
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      react: require.resolve('react'),
-      'react-dom': require.resolve('react-dom'),
-    };
-    return config;
-  },
 };
 
 module.exports = nextConfig;
